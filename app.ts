5929702ba783978cc739dcb382ae4b4248f6c66b
Francois Pakkanen <97538687+FrankPak@users.noreply.github.com>
2025-11-17 08:45:43 +0200

    First init

---
+++ app.ts
@@ -0,0 +1,14 @@
+import express, {type Express} from 'express'
+import path  from "path"
+const app: Express = express()
+const port = 3000
+//const __dirname = path.resolve()
+
+//app.use(express.static(path.join(__dirname, "../public")))
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
