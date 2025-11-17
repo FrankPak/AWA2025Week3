8a163d5213f21b9de64f4204cd49404e3e76bbd1
Francois Pakkanen <97538687+FrankPak@users.noreply.github.com>
2025-11-17 08:51:28 +0200

    added fixes for /hello

--- app.ts
+++ app.ts
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/hello', (req, res) => {
+  res.send('Hello World!')
+})
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
--- dist/app.js
+++ dist/app.js
@@ -11,6 +11,9 @@ const port = 3000;
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+app.get('/hello', (req, res) => {
+    res.send('Hello World!');
+});
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
